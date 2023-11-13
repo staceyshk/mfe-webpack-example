ada50ee828c85084bfc91ea94d873338cc2906e0
Stacey Shkuratoff <staceyshk@gmail.com>
2023-11-12 18:59:29 -0700

    new example

--- env-file-example/app1/src/App.tsx
+++ env-file-example/app1/src/App.tsx
@@ -11,30 +11,17 @@ const MainPage = () => {
           '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
       }}
     >
-    <h1>React Dynamic Routes</h1>
+    <h1>Webpack Module Federation Example 2 - Build Time Remotes with Env Files</h1>
     <h2>App 1</h2>
     <p>
-      The Dynamic Routes will take advantage Module Federation <strong>remotes</strong> and{' '}
-      <strong>exposes</strong>. It will not load any components or modules that have been loaded
-      already. It will create the routes at runtime from a json list. The goal is to support a list
-      of remotes in a registry, such that we don't need to redeploy the host when we add new remotes.
+      The Build Time Remotes with Env Files example will take advantage Module Federation <strong>remotes</strong> and{' '}
+      <strong>exposes</strong>. The remote locations are included with an env file. You can use multiple env files during the build to change the location of remotes.
     </p>
     <Link to='app2'>Go to App 2</Link>
     <br />
     <Link to='app3'>Go to App 3</Link>
     <br />
   </div>)
-}
-
-const AppComponent = (elementJson: any, index: number) => {
-  const RemoteComponent = React.lazy(() => importRemote(
-    { url: elementJson.url,
-      scope: elementJson.scope,
-      module: elementJson.module
-    })
-  );
-  const path = `${elementJson.scope}/*`;
-  return (<Route key={index} path={path} element={ <RemoteComponent /> } />); 
 }
 
 function App() {
--- env-file-example/app1/webpack.config.js
+++ env-file-example/app1/webpack.config.js
@@ -18,6 +18,7 @@ module.exports = (env) => {
       },
       port: 3001,
       historyApiFallback: true,
+      open: true,
     },
     output: {
       publicPath: 'auto',
